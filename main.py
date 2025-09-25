f77954538c6403274139a2d05c496a9a1372a0cc
Mahesh Venkatesh Shet <165457859+mahesh-from-sirsi@users.noreply.github.com>
2025-09-25 20:13:43 +0530

    Update main.py
    Concurrency fix with human message

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import time
 from langchain_openai import ChatOpenAI
 from concurrent.futures import ThreadPoolExecutor
+from langchain_core.messages import HumanMessage
 
 # Title and description
 st.title("🤴🤴🤴 MaheshVShet's-FineTuned-LinkedIn-GPT 🤴🤴🤴")
@@ -12,19 +13,21 @@ st.markdown("🔥🔥🔥 Powered by GPT-4.1 (gpt-4.1-mini-2025-04-14) fine-tune
 topic = st.text_input("Please enter the topic")
 
 st.code("""
-            Example:
-            Explain Transformers Architecture
-            How does RAG work?
-            """, language=None)
+    Example:
+    Explain Transformers Architecture
+    How does RAG work?
+    """, language=None)
 
 # Initialize models
 base_model = ChatOpenAI(model="gpt-4.1-mini-2025-04-14")
 ft_model = ChatOpenAI(model="ft:gpt-4.1-mini-2025-04-14:daivajnaads:maheshvs-linkedinposts:CJZ5hLYj")
 
+# Function to generate responses concurrently
 def generate_linkedin_post(prompt):
+    human_prompt = [HumanMessage(content=prompt)]
     with ThreadPoolExecutor() as executor:
-        f1 = executor.submit(base_model.invoke, prompt)
-        f2 = executor.submit(ft_model.invoke, prompt)
+        f1 = executor.submit(base_model.invoke, human_prompt)
+        f2 = executor.submit(ft_model.invoke, human_prompt)
         response1 = f1.result()
         response2 = f2.result()
     return response1.content, response2.content
